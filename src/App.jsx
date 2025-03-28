ec6f6f8176ce1d6d94dba51a82d487bf1660ea4a
MOHAMED ACHAROUAOU <acharouaoumohamed@gmail.com>
2025-03-28 00:04:51 +0000

    add chat context and navigation guard to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import AppRoutes from "./routes/AppRoutes.jsx";
+import { ChatProvider } from "./pages/User/chat/components/ChatContext.jsx";
 import {AuthProvider} from "./context/Auth/AuthContext.jsx";
 import {BrowserRouter as Router} from "react-router-dom";
+import NavigationGuard from "./pages/User/chat/components/NavigationGuard.jsx";
 
 const App = () => {
     return (
         <Router>
         <AuthProvider>
+            <ChatProvider>
+                <NavigationGuard />
             <AppRoutes />
+            </ChatProvider>
         </AuthProvider>
         </Router>
     );
